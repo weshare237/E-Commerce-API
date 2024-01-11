27d5a73f6d327caf47d498a885f1648ce303a005
FOPA KUETE DUCLAIR <fopaduclair2000@dimsoft.eu>
2024-01-11 14:16:40 +0100

    Started implementing user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,24 @@
 const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
 
 const register = async (req, res) => {
-  res.send('user registration')
+  const { email, name, password } = req.body
+
+  const emailAlreadyTaken = await User.findOne({ email })
+
+  if (emailAlreadyTaken) {
+    throw new CustomError.BadRequestError('Email already taken')
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0
+
+  const role = isFirstAccount ? 'admin' : 'user'
+
+  const user = await User.create({ email, name, password, role })
+
+  res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
